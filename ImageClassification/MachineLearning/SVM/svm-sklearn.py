25e96a0d1efcf32b2765850d111f1f8ca67abae1
Aweozzz <137423497@qq.com>
2023-02-12 23:34:45 +0800

    first commit

---
+++ ImageClassification/MachineLearning/SVM/svm-sklearn.py
@@ -0,0 +1,3 @@
+# Something interesting about AI
+Firstly for finish my graduation design,I created this respository
+Maybe more self-learning code will be pushed in the future if I am not that lazy-.-
